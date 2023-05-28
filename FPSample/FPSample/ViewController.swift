7f2e45b410fa6b836f0f5f28817d28f2bb558b5b
mason <myssun0325@gmail.com>
2023-05-28 16:17:10 +0900

    init

---
+++ FPSample/FPSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FPSample
+//
+//  Created by Mason on 2023/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
